9584d0843d1b1a2228eb9e9c824131ecb694b157
Abdelrahman Abdellatif <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2021-11-08 23:15:58 +0300

    Update Titanic.R

--- Q2 Titanic/Titanic.R
+++ Q2 Titanic/Titanic.R
@@ -111,3 +111,66 @@ plot(rules.sorted, method = "graph", control = list(type = "items"))
 
 #ref : Association Rule Mining with R∗ 
 #https://www.webpages.uidaho.edu/~stevel/517/RDM-slides-association-rule-mining-with-r.pdf
+
+
+
+
+
+#The Titanic dataset in the datasets package is a 4-dimensional
+#table with summarized information on the fate of passengers
+#on the Titanic according to social class, sex, age and survival
+
+install.packages("arules")
+install.packages("ggplot2")
+install.packages("plyr")
+install.packages("ROCR")
+
+
+library(ggplot2)
+library(plyr)
+library(ROCR)
+library(arules)
+
+load("titanic.raw.rdata")
+
+df <- as.data.frame(Titanic)
+idx <- sample(1:nrow(titanic.raw), 5)
+titanic.raw[idx, ]
+summary(titanic.raw)
+
+
+library(arules)
+
+rules.all## number of rules discovered
+
+rules.all ## %>% inspect() ## print all rules
+
+rules.all <- apriori(titanic.raw)
+
+dim(titanic.raw)
+
+rules.all <- apriori(titanic.raw) ## rules.all <- titanic.raw %>% apriori() ## run the APRIORI algorithm
+
+inspect(rules.all) ## rules.all %>% inspect() ## print all rules
+
+rules <- apriori(titanic.raw,
+                parameter=list(minlen=2, supp=0.001, conf=0.6),  
+                appearance = list(default = "lhs", rhs=c("Survived=Yes","Survived=No")))
+
+inspect(rules.all)
+
+rules.sorted <- sort(rules, by="lift")
+
+
+inspect(rules.all)
+
+require(arulesViz)
+
+
+
+plot(rules.sorted, method="graph", control=list(nodeCol="red", edgeCol="blue"))
+
+plot(rules.sorted)
+plot(rules.sorted, method="grouped")
+plot(rules.sorted, method = "paracoord", control = list(reorder = TRUE))
+plot(rules.sorted, method = "graph", control = list(type = "items"))
